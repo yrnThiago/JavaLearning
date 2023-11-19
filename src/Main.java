1657bd3b260d3f8d07c33b725e0d8264ad983f52
yrnThiago <thiagodias_123@hotmail.com>
2023-11-19 19:06:46 -0300

    feat: primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
